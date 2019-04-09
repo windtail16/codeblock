e43a477e2f8b3506d50a8fb6cfa18b8cc2fab5ee
windtail16 <windtail16@naver.com>
2019-04-09 16:01:52 +0900

    메인 수정 사항 반영

---
+++ common/js/script.js
@@ -0,0 +1,29 @@
+$('.smooth-link').click(function() {
+  var target = $(this.hash);
+  target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+  if (target.length) {
+    $('html,body').animate({
+      scrollTop: target.offset().top - 80
+    }, 1000);
+  }
+})
+// TOP go btn
+$('.top-go').click(function () {
+  $('html, body').animate({
+    scrollTop: '0px'
+  },
+  1500);
+  return false;
+});
+$('.bottom-go').click(function () {
+  $('html, body').animate({
+    scrollTop: $(document).height()
+  },
+  1500);
+  return false;
+});
+//menu mobile
+$('.btn-menu').click(function() {
+  $(this).next('ul.dl-menu').toggleClass('invisible');
+  
+});
