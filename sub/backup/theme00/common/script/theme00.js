7afacaa14d0259a17915311b3ed2b9c7bd3b83b2
windtail16 <windtail16@naver.com>
2019-06-26 11:49:10 +0900

    테마 2번 작업중

---
+++ sub/backup/theme00/common/script/theme00.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  var winWidth = $(window).width();
+  
+  // 2Depth Menu
+  if (winWidth >= 980) {
+    $(".gnb").hover(function () {
+      $(".gnb .dropdown-menu").toggleClass("show");
+      $(".gnb .gnb-bg").toggleClass("show");
+    });
+
+    $( ".gnb" ).mouseleave(function() {
+      $(".gnb .dropdown-menu").removeClass("show");
+      $(".gnb .gnb-bg").removeClass("show");  
+    }); 
+  }
+  // 2Depth Menu for Resize
+  $(window).resize(function(){
+    if (winWidth >= 980) {
+      $(".gnb").hover(function () {
+        $(".gnb .dropdown-menu").toggleClass("show");
+        $(".gnb .gnb-bg").toggleClass("show");
+      });
+
+      $( ".gnb" ).mouseleave(function() {
+        $(".gnb .dropdown-menu").removeClass("show");
+        $(".gnb .gnb-bg").removeClass("show");  
+      }); 
+    }
+  });
+  
+});
